6c0815534118420fc0f40640f546b2492541dba6
Alex Tetervak <tetervak@sheridancollege.ca>
2021-02-22 08:05:34 -0500

    initial commit

---
+++ src/main/java/ca/javateacher/passwordencoder/controller/PasswordController.java
@@ -0,0 +1,40 @@
+package ca.javateacher.passwordencoder.controller;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PasswordController {
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    public PasswordController(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping(value={"/","/Input"})
+    public String input(){
+        return "Input";
+    }
+
+    @GetMapping("/Encode")
+    public ModelAndView encode(@RequestParam String password){
+        if(password == null || password.isEmpty()){
+            return new ModelAndView("Input",
+                    "message","The password is left empty");
+        }else{
+            String[] encoded = new String[10];
+            for(int ind = 0; ind < encoded.length; ind++){
+                encoded[ind] = passwordEncoder.encode(password);
+            }
+            ModelAndView mv = new ModelAndView("Output");
+            mv.addObject("password", password);
+            mv.addObject("encoded", encoded);
+            return mv;
+        }
+    }
+}
---
+++ src/test/java/ca/javateacher/passwordencoder/PasswordEncoderApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.javateacher.passwordencoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordEncoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
